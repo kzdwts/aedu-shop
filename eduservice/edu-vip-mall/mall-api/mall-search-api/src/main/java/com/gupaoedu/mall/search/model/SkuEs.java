5527e5ca4c3d9e7fb65be29cc4fc750dbfbf089e
kangyong <kangyong_wts@sina.com>
2022-02-17 18:24:51 +0800

    feat: 初始化搜索api工程

---
+++ eduservice/edu-vip-mall/mall-api/mall-search-api/src/main/java/com/gupaoedu/mall/search/model/SkuEs.java
@@ -0,0 +1,16 @@
+package com.gupaoedu.mall.search.model;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * 商品搜索参数组合类
+ *
+ * @author Kang Yong
+ * @date 2022/2/17
+ * @since 1.0.0
+ */
+@Data
+@ToString
+public class SkuEs {
+}
