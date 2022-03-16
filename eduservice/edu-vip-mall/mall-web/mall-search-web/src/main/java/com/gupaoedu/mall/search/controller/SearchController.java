7169092dbf9e10089e27124516caec946a5af019
kangyong <kangyong_wts@sina.com>
2022-03-16 22:09:13 +0800

    feat: 页面渲染

--- eduservice/edu-vip-mall/mall-web/mall-search-web/src/main/java/com/gupaoedu/mall/search/controller/SearchController.java
+++ eduservice/edu-vip-mall/mall-web/mall-search-web/src/main/java/com/gupaoedu/mall/search/controller/SearchController.java
@@ -2,6 +2,7 @@ package com.gupaoedu.mall.search.controller;
 
 import com.gupaoedu.mall.search.feign.SkuSearchFeign;
 import com.gupaoedu.mall.util.RespResult;
+import com.gupaoedu.mall.util.UrlUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -40,6 +41,11 @@ public class SearchController {
         // 数据存入model
         model.addAttribute("result", respResult.getData());
         model.addAttribute("searchMap", searchMap);
+
+        // 当前地址
+        model.addAttribute("url", UrlUtils.map2url("/web/search", searchMap));
+        // 没有排序参数的url
+        model.addAttribute("urlsort", UrlUtils.replateUrlParameter(model.getAttribute("url").toString(), "sfield", "sm"));
 
         return "search";
     }
