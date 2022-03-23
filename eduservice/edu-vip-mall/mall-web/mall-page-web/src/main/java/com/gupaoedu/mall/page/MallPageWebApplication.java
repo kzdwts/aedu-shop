c91e70335c7cdb5cff11cea525cddc0bfcceb1b5
kangyong <kangyong_wts@sina.com>
2022-03-23 14:54:54 +0800

    feat: 初始化商品详情页工程配置

---
+++ eduservice/edu-vip-mall/mall-web/mall-page-web/src/main/java/com/gupaoedu/mall/page/MallPageWebApplication.java
@@ -0,0 +1,20 @@
+package com.gupaoedu.mall.page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author Kang Yong
+ * @date 2022/3/23
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class MallPageWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallPageWebApplication.class, args);
+    }
+
+}
