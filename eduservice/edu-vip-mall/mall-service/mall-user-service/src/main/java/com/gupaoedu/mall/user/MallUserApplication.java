7870c45765e4e9154f2949a433427ef532a57ad8
kangyong <kangyong_wts@sina.com>
2022-04-18 16:24:50 +0800

    feat: 初始化用户中心

---
+++ eduservice/edu-vip-mall/mall-service/mall-user-service/src/main/java/com/gupaoedu/mall/user/MallUserApplication.java
@@ -0,0 +1,20 @@
+package com.gupaoedu.mall.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 用户中心
+ *
+ * @author Kang Yong
+ * @date 2022/4/18
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class MallUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallUserApplication.class, args);
+    }
+
+}
