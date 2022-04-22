a86eb60cb9e0ac80f2c56cdbad72636d87530a68
kangyong <kangyong_wts@sina.com>
2022-04-22 17:50:04 +0800

    feat: 初始化订单工程

---
+++ eduservice/edu-vip-mall/mall-service/mall-order-service/src/main/java/com/gupaoedu/mall/MallOrderApplication.java
@@ -0,0 +1,29 @@
+package com.gupaoedu.mall;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import javax.swing.*;
+
+/**
+ * 订单
+ *
+ * @author Kang Yong
+ * @date 2022/4/22
+ * @since 1.0.0
+ */
+@SpringBootApplication
+@MapperScan(basePackages = "com.gupaoedu.mall.order.mapper")
+@EnableFeignClients(basePackages = {
+        "com.gupaoedu.mall.goods.feign", // 商品
+        "com.gupaoedu.mall.cart.feign", // 购物车
+})
+public class MallOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallOrderApplication.class, args);
+    }
+
+}
