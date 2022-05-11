76c49d8e4eeab31159e1d562ce7ed8c2797750b9
kangyong <kangyong_wts@sina.com>
2022-05-11 09:26:28 +0800

    feat: 记录支付日志

---
+++ eduservice/edu-vip-mall/mall-service/mall-pay-service/src/main/java/com/gupaoedu/mall/pay/mapper/PayLogMapper.java
@@ -0,0 +1,14 @@
+package com.gupaoedu.mall.pay.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.gupaoedu.mall.pay.model.PayLog;
+
+/**
+ * @author Kang Yong
+ * @date 2022/5/11
+ * @since 1.0.0
+ */
+public interface PayLogMapper extends BaseMapper<PayLog> {
+
+
+}
---
+++ eduservice/edu-vip-mall/mall-service/mall-pay-service/src/main/java/com/gupaoedu/mall/pay/service/impl/PayLogServiceImpl.java
@@ -0,0 +1,36 @@
+package com.gupaoedu.mall.pay.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.gupaoedu.mall.pay.mapper.PayLogMapper;
+import com.gupaoedu.mall.pay.model.PayLog;
+import com.gupaoedu.mall.pay.service.PayLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 支付日志
+ *
+ * @author Kang Yong
+ * @date 2022/5/11
+ * @since 1.0.0
+ */
+@Service
+public class PayLogServiceImpl extends ServiceImpl<PayLogMapper, PayLog> implements PayLogService {
+
+    @Autowired
+    private PayLogMapper payLogMapper;
+
+    /**
+     * 记录日志
+     *
+     * @param payLog {@link PayLog}
+     * @author Kang Yong
+     * @date 2022/5/11
+     */
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public void log(PayLog payLog) {
+        this.payLogMapper.insert(payLog);
+    }
+}
