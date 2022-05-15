d6a5fa85647a20cb3cd45c5398dd0fbee6bebb75
kangyong <kangyong_wts@sina.com>
2022-05-15 11:39:18 +0800

    feat: AES加密解密

---
+++ eduservice/edu-vip-mall/mall-util/mall-common/src/main/java/com/gupaoedu/mall/util/AESUtil.java
@@ -0,0 +1,42 @@
+package com.gupaoedu.mall.util;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.Security;
+
+/**
+ * AES 加密/解密
+ *
+ * @author Kang Yong
+ * @date 2022/5/15
+ * @since 1.0.0
+ */
+public class AESUtil {
+
+    /**
+     * AES 加密/解密
+     *
+     * @param buffer    {@link byte[]} 明文/密文
+     * @param appsecret {@link String} 秘钥，16位
+     * @param mode      {@link Integer} 处理方式 1：加密，2：解密
+     * @return {@link byte[]}
+     * @author Kang Yong
+     * @date 2022/5/15
+     */
+    public static byte[] encryptAndDecrypt(byte[] buffer, String appsecret, Integer mode) throws Exception {
+        // 1、加载加密处理对象，该对象会提供加密算法、密钥生成、秘钥转换、秘钥管理等功能
+        Security.addProvider(new BouncyCastleProvider());
+        // 2、创建秘钥对象，并指定算法
+        SecretKeySpec secretKey = new SecretKeySpec(appsecret.getBytes(StandardCharsets.UTF_8), "AES");
+        // 3、设置Cipher的加密模式，AES/ECP/PKCS7Padding BC指定算法对象（）
+        Cipher cipher = Cipher.getInstance("AES/ECP/PKCS7Padding", "BC");
+        // 4、初始化加密配置
+        cipher.init(mode, secretKey);
+        // 5、执行加密/解密
+        return cipher.doFinal(buffer);
+    }
+
+}
