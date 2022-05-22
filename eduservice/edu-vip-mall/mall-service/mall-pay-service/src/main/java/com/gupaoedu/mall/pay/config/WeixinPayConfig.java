4a385aba2f9faffb916471ce463d4fbe46ded190
kangyong <kangyong_wts@sina.com>
2022-05-22 09:30:12 +0800

    feat: wx配置类更新

--- eduservice/edu-vip-mall/mall-service/mall-pay-service/src/main/java/com/gupaoedu/mall/pay/config/WeixinPayConfig.java
+++ eduservice/edu-vip-mall/mall-service/mall-pay-service/src/main/java/com/gupaoedu/mall/pay/config/WeixinPayConfig.java
@@ -2,10 +2,15 @@ package com.gupaoedu.mall.pay.config;
 
 import com.github.wxpay.sdk.IWXPayDomain;
 import com.github.wxpay.sdk.WXPayConfig;
+import com.github.wxpay.sdk.WXPayConstants;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.Objects;
 
 /**
  * 微信支付配置
@@ -67,13 +72,57 @@ public class WeixinPayConfig extends WXPayConfig {
         return this.key;
     }
 
+    /**
+     * 获取商户整数内容
+     *
+     * @return {@link InputStream}
+     * @author Kang Yong
+     * @date 2022/5/22
+     */
     @Override
     public InputStream getCertStream() {
-        return null;
+        // 加载整数
+        if (Objects.isNull(certData)) {
+            synchronized (WeixinPayConfig.class) {
+                try {
+                    if (Objects.isNull(certData)) {
+                        File file = new File(certPath);
+                        InputStream certStream = new FileInputStream(file);
+                        this.certData = new byte[(int) file.length()];
+                        certStream.read(this.certData);
+                        certStream.close();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        ByteArrayInputStream certBis = new ByteArrayInputStream(this.certData);
+        return certBis;
     }
 
+    /**
+     * 获取IWXPayDomain， 用于多域名容灾自动切换
+     *
+     * @return {@link IWXPayDomain}
+     * @author Kang Yong
+     * @date 2022/5/22
+     */
     @Override
     public IWXPayDomain getWXPayDomain() {
-        return null;
+        // 这个方法需要这样实现，否咋无法正常初始化WXPay
+        IWXPayDomain iwxPayDomain = new IWXPayDomain() {
+            @Override
+            public void report(String s, long l, Exception e) {
+
+            }
+
+            @Override
+            public DomainInfo getDomain(WXPayConfig wxPayConfig) {
+                return new IWXPayDomain.DomainInfo(WXPayConstants.DOMAIN_API, true);
+            }
+        };
+        return iwxPayDomain;
     }
 }
