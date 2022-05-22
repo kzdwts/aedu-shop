c27973cfd831a0ab3ba460a25c6cf85958f1908c
kangyong <kangyong_wts@sina.com>
2022-05-22 09:35:03 +0800

    feat: 微信支付交给spring容器管理

--- eduservice/edu-vip-mall/mall-service/mall-pay-service/src/main/java/com/gupaoedu/mall/MallPayApplication.java
+++ eduservice/edu-vip-mall/mall-service/mall-pay-service/src/main/java/com/gupaoedu/mall/MallPayApplication.java
@@ -1,8 +1,11 @@
 package com.gupaoedu.mall;
 
+import com.github.wxpay.sdk.WXPay;
+import com.gupaoedu.mall.pay.config.WeixinPayConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * 启动
@@ -17,6 +20,19 @@ public class MallPayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MallPayApplication.class, args);
+    }
+
+    /**
+     * 微信支付SDK对象
+     *
+     * @param weixinPayConfig {@link WeixinPayConfig}
+     * @return {@link WXPay}
+     * @author Kang Yong
+     * @date 2022/5/22
+     */
+    @Bean
+    public WXPay wxPay(WeixinPayConfig weixinPayConfig) throws Exception {
+        return new WXPay(weixinPayConfig);
     }
 
 }
