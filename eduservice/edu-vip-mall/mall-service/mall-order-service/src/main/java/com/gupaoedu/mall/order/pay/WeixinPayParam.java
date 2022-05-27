7e7843f17d916efa77cf0fe6467fe1e19825e5b3
kangyong <kangyong_wts@sina.com>
2022-05-27 23:50:19 +0800

    feat: 定义微信支付参数封装方法

--- eduservice/edu-vip-mall/mall-service/mall-order-service/src/main/java/com/gupaoedu/mall/order/pay/WeixinPayParam.java
+++ eduservice/edu-vip-mall/mall-service/mall-order-service/src/main/java/com/gupaoedu/mall/order/pay/WeixinPayParam.java
@@ -1,8 +1,11 @@
 package com.gupaoedu.mall.order.pay;
 
+import com.gupaoedu.mall.order.model.Order;
 import com.gupaoedu.mall.util.Signature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 微信支付参数
@@ -16,5 +19,20 @@ public class WeixinPayParam {
 
     @Autowired
     private Signature signature;
+
+    /**
+     * 微信支付参数封装
+     * 对参数进行签名
+     * 对整体参数进行加密
+     *
+     * @param order   {@link Order} 订单数据
+     * @param request {@link HttpServletRequest}
+     * @return {@link String}
+     * @author Kang Yong
+     * @date 2022/5/27
+     */
+    public String weixinParam(Order order, HttpServletRequest request) {
+        return null;
+    }
 
 }
