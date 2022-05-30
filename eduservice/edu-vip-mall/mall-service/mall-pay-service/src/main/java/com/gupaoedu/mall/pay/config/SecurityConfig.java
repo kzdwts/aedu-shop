c2ef7f8e20d6fca4daca34a563babd3a34104723
kangyong <kangyong_wts@sina.com>
2022-05-30 09:11:23 +0800

    feat: 安全配置

---
+++ eduservice/edu-vip-mall/mall-service/mall-pay-service/src/main/java/com/gupaoedu/mall/pay/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.gupaoedu.mall.pay.config;
+
+import com.gupaoedu.mall.util.Signature;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * 初始化signature配置
+ *
+ * @author Kang Yong
+ * @date 2022/5/25
+ * @since 1.0.0
+ */
+@Configuration
+public class SecurityConfig {
+
+
+    /**
+     * 秘钥
+     */
+    @Value("${payconfig.aes.skey}")
+    private String skey;
+
+    /**
+     * 验签加密盐
+     */
+    @Value("${payconfig.aes.salt}")
+    private String salt;
+
+    /**
+     * 验签对象
+     *
+     * @return {@link Signature}
+     * @author Kang Yong
+     * @date 2022/5/25
+     */
+    @Bean(value = "signature")
+    public Signature signature() {
+        return new Signature(skey, salt);
+    }
+
+}
