aa75562bc2bab2a59a29c4f139fa6e71b05b23e6
kangyong <kangyong_wts@sina.com>
2022-06-09 09:14:46 +0800

    feat: 支付结果监听

--- eduservice/edu-vip-mall/mall-service/mall-order-service/src/main/java/com/gupaoedu/mall/order/mq/OrderResultListener.java
+++ eduservice/edu-vip-mall/mall-service/mall-order-service/src/main/java/com/gupaoedu/mall/order/mq/OrderResultListener.java
@@ -1,7 +1,10 @@
 package com.gupaoedu.mall.order.mq;
 
 import com.gupaoedu.mall.order.service.OrderService;
+import org.apache.rocketmq.spring.core.RocketMQLocalTransactionListener;
+import org.apache.rocketmq.spring.core.RocketMQLocalTransactionState;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
 /**
@@ -12,8 +15,18 @@ import org.springframework.stereotype.Component;
  * @since 1.0.0
  */
 @Component
-public class OrderResultListener {
+public class OrderResultListener implements RocketMQLocalTransactionListener {
 
     @Autowired
     private OrderService orderService;
+
+    @Override
+    public RocketMQLocalTransactionState executeLocalTransaction(Message message, Object o) {
+        return null;
+    }
+
+    @Override
+    public RocketMQLocalTransactionState checkLocalTransaction(Message message) {
+        return null;
+    }
 }
