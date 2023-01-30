0e3949c7773e6ab7369a9b810c9e496c498e28b0
kangyong <kangyong_wts@sina.com>
2023-01-30 22:37:34 +0800

    feat: 新增订单退款申请

---
+++ eduservice/edu-vip-mall/mall-service/mall-order-service/src/main/java/com/gupaoedu/mall/order/mapper/OrderRefundMapper.java
@@ -0,0 +1,18 @@
+package com.gupaoedu.mall.order.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.gupaoedu.mall.order.model.OrderRefund;
+
+/**
+ * @author KY
+ * @description 针对表【order_refund(订单退款记录表)】的数据库操作Mapper
+ * @createDate 2023-01-30 21:51:18
+ * @Entity generator.domain.OrderRefund
+ */
+public interface OrderRefundMapper extends BaseMapper<OrderRefund> {
+
+}
+
+
+
+
