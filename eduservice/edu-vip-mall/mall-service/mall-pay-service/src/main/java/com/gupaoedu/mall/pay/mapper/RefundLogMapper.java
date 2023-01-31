44236a7a016f3c51189be175c9190aabefa0dbe7
kangyong <kangyong_wts@sina.com>
2023-01-31 22:32:22 +0800

    feat: 申请退款日志记录

---
+++ eduservice/edu-vip-mall/mall-service/mall-pay-service/src/main/java/com/gupaoedu/mall/pay/mapper/RefundLogMapper.java
@@ -0,0 +1,18 @@
+package com.gupaoedu.mall.pay.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.gupaoedu.mall.pay.model.RefundLog;
+
+/**
+ * @author KY
+ * @description 针对表【refund_log(退款申请记录表)】的数据库操作Mapper
+ * @createDate 2023-01-31 22:19:12
+ * @Entity generator.domain.RefundLog
+ */
+public interface RefundLogMapper extends BaseMapper<RefundLog> {
+
+}
+
+
+
+
