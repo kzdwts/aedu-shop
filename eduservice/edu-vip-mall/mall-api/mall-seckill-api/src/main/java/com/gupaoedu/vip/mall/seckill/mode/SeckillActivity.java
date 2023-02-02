1dffbb98e18a003c8f18d116cbdd81b4a239bffc
kangyong <kangyong_wts@sina.com>
2023-02-02 22:13:13 +0800

    feat: 新增秒杀工程

---
+++ eduservice/edu-vip-mall/mall-api/mall-seckill-api/src/main/java/com/gupaoedu/vip/mall/seckill/mode/SeckillActivity.java
@@ -0,0 +1,11 @@
+package com.gupaoedu.vip.mall.seckill.mode;
+
+/**
+ * 秒杀活动
+ *
+ * @author Kang Yong
+ * @date 2023/2/2
+ * @since 1.0.0
+ */
+public class SeckillActivity {
+}
---
+++ eduservice/edu-vip-mall/mall-service/mall-seckill-service/src/main/java/com/gupaoedu/mall/seckill/SeckillApplication.java
@@ -0,0 +1,20 @@
+package com.gupaoedu.mall.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * start
+ *
+ * @author Kang Yong
+ * @date 2023/2/2
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class SeckillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillApplication.class, args);
+    }
+
+}
