8568f688942b13081700d05fb2ce759847a46d65
kangyong <kangyong_wts@sina.com>
2023-02-06 21:49:59 +0800

    feat: canal引入elasticjob

---
+++ eduservice/edu-vip-mall/mall-service/mall-canal-service/src/main/java/com/gupaoedu/canal/task/statictask/SyncStaticTask.java
@@ -0,0 +1,29 @@
+package com.gupaoedu.canal.task.statictask;
+
+import com.dangdang.ddframe.job.api.ShardingContext;
+import com.dangdang.ddframe.job.api.simple.SimpleJob;
+import com.dangdang.elasticjob.lite.annotation.ElasticSimpleJob;
+import org.springframework.stereotype.Component;
+
+/**
+ * 同步静态任务
+ *
+ * @author Kang Yong
+ * @date 2023/2/6
+ * @since 1.0.0
+ */
+@Component
+@ElasticSimpleJob(
+        cron = "0/10 * * * * ?",
+        jobName = "synctask",
+        shardingTotalCount = 1
+)
+public class SyncStaticTask implements SimpleJob {
+
+
+    @Override
+    public void execute(ShardingContext shardingContext) {
+        System.out.println("执行任务。。。");
+    }
+
+}
