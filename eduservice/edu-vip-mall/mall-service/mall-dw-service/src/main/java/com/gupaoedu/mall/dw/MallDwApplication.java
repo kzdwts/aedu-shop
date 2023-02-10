cd420516e065508b86abc1f662f84c5901d2a2f2
kangyong <kangyong_wts@sina.com>
2023-02-10 22:35:54 +0800

    feat: 新增dw系统启动类

---
+++ eduservice/edu-vip-mall/mall-service/mall-dw-service/src/main/java/com/gupaoedu/mall/dw/MallDwApplication.java
@@ -0,0 +1,20 @@
+package com.gupaoedu.mall.dw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * start
+ *
+ * @author Kang Yong
+ * @date 2023/2/10
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class MallDwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallDwApplication.class, args);
+    }
+
+}
