0e4aed94c2c07a84adb06fefda1d42b798457ced
kangyong <kangyong_wts@sina.com>
2023-04-21 15:25:43 +0800

    feat: 网关

---
+++ eduservice/edu-vip-mall/mall-gateway/mall-api-gateway/src/main/java/com/gupaoedu/mall/api/hot/HotQueue.java
@@ -0,0 +1,14 @@
+package com.gupaoedu.mall.api.hot;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 热门商品排队
+ *
+ * @author Kang Yong
+ * @date 2023/4/21
+ * @since 1.0.0
+ */
+@Component
+public class HotQueue {
+}
