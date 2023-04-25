6f467610b930c60266238c6fb082e2ff926046c1
kangyong <kangyong_wts@sina.com>
2023-04-25 15:59:13 +0800

    feat: 过滤

--- eduservice/edu-vip-mall/mall-gateway/mall-api-gateway/src/main/java/com/gupaoedu/mall/api/filter/RequestFilter.java
+++ eduservice/edu-vip-mall/mall-gateway/mall-api-gateway/src/main/java/com/gupaoedu/mall/api/filter/RequestFilter.java
@@ -1,14 +1,20 @@
 package com.gupaoedu.mall.api.filter;
 
+import com.alibaba.fastjson.JSON;
 import com.gupaoedu.mall.api.hot.HotQueue;
+import com.gupaoedu.mall.domain.customEnum.HotGoodsQCodeEnum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 过滤器
@@ -46,7 +52,19 @@ public class RequestFilter implements GlobalFilter, Ordered {
         // 排队结果
         int result = hotQueue.hotToQueue(userName, goodsId, num);
 
-        // TODO
+        // QUEUE_ING、HAS_QUEUE
+        if (result == HotGoodsQCodeEnum.QUEUE_ING.getCode() || result == HotGoodsQCodeEnum.HAS_QUEUE.getCode()) {
+            Map<String, Object> resultMap = new HashMap<String, Object>();
+            resultMap.put("type", "hot");
+            resultMap.put("code", result);
+
+            exchange.getResponse().setStatusCode(HttpStatus.OK);
+            exchange.getResponse().setComplete();
+
+            exchange.getResponse().getHeaders().add("message", JSON.toJSONString(resultMap));
+        }
+
+        // NOT_HOT 直接由后端服务处理
         return chain.filter(exchange);
     }
 
